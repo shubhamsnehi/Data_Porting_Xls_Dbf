6f97e97ee32598f759bd1a30101afb8317f9dce6
Shubham <dev@aiocdawacs.com>
2021-05-14 18:04:37 +0530

    small correction 5

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ if __name__ == '__main__':
         awacslogger, sourcefile, dataobj))
 
 # py main.py -p gs://balatestawacs/SampleFiles/AIOCD0923/AIOCD0923_02_2021_511b9d2d-76c3-4e4e-a2a4-35840fc612ce.xls --dpath /Tempfiles2/pqr.csv
-# py main.py -p gs://balatestawacs/SALE_DTL.DBF --dpath /Tempfiles3/abc.csv
+# py main.py -p gs://balatestawacs/SALE_DTL.DBF --dpath /Tempfiles3/abcd.csv
